5d44b87f1a4605b30ed2126b1c758c13375cdcaf
Vitalii SYVENIUK <vsyveniu@e2r7p5.unit.ua>
2018-02-02 15:34:20 +0200

    sdf

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -78,16 +78,19 @@ void	ft_putdoublecratch(f_list *p, char c2, int size, int size2);
 void	ft_printhash(char c);
 int		ft_checkmodifiers(f_list *p);
 int		ft_checkflag(f_list *p, void *arg, int size, int val);
-int	ft_handleleft(f_list *p, int arg, int size, int val);
-int	ft_handleright(f_list *p, int arg, int size);
-void	ft_handleneg(f_list *p, int arg, int size);
-void	ft_handlepos(f_list *p, int arg, int size);
+int	ft_handleleft(f_list *p,  int size);
+int	ft_handleright(f_list *p,  int size);
+void	ft_handleneg(f_list *p, int size);
+void	ft_handlepos(f_list *p, int size);
 int		ft_getargsize(f_list *p, void *arg);
 int 	ft_getsizehexoctbi(f_list *p, void *arg);
 int		ft_checkmodifiers(f_list *p);
 int		ft_getargsize(f_list *p, void *arg);
 int 	ft_getsizehexoctbi(f_list *p, void *arg);
 int 	ft_printf(const char *format, ... );
+int	ft_printsize(f_list *p, unsigned long size);
+int		ft_printstrsize(f_list *p, unsigned long size);
+int		ft_checkispossibletoputthisbitchinchar(const char *format, int i);
 
 
 
